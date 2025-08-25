ce82c8ec15ff3530024e72eace4285bf4f537b3a
Rodrigo-F-Nunes <odeioestargripado@gmail.com>
2025-08-25 01:37:00 -0300

    Add files via upload

---
+++ LengthOfLastWordSol.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size()-1;
+        int j;
+        while(i >= 0 && s[i] == ' '){
+         i--;
+        }
+
+        j = i;
+
+        while(j >= 0 && s[j] != ' '){
+         j--;
+        }
+        
+        return i - j;
+    }
+};
